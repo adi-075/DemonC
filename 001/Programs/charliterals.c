a1d7ba93a059c284aa4eb45efa17ed3acd077d00
Adi-075 <adityamhambrey323@gmail.com>
2021-09-29 09:47:12 +0530

    Add Character Literals Program

---
+++ 001/Programs/charliterals.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  char ch = 'a';
+  printf("Value of ch is = %c \n", ch);
+
+  ch = '$';
+  printf("Value of ch is = %c \n", ch);
+
+  ch = '6';
+  printf("Value of ch is = %c \n", ch);
+
+
+  ch = '\'';
+  printf("Value of ch is = %c \n", ch);
+
+
+  ch = '\"';
+  printf("Value of ch is = %c \n", ch);
+
+
+  ch = '\\';
+  printf("Value of ch is = %c \n", ch);
+
+  printf("Hello, World!");
+  return 0;
+}
