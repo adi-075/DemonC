0ee02688333d1b9a2f5c5fe4c8175cc92b348e46
Adi-075 <adityamhambrey323@gmail.com>
2021-09-29 10:08:42 +0530

    Add Datatype Program

---
+++ 001/Programs/datatype.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+  int num = 10;
+  char ch = 'Y';
+  float amount = 505.43;
+  double salary = 100.93;
+  
+  printf("num = %d \n", num);
+  printf("ch = %c \n", ch);
+  printf("amount = %f \n", amount);
+  printf("salary = %lf", salary);
+
+  return 0;
+}
