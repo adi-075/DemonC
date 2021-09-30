6376428417c6e7d053a9af6bf9cec0db292a2cc3
Adi-075 <adityamhambrey323@gmail.com>
2021-09-30 12:28:59 +0530

    Add Input and Output Programs

---
+++ 001/Programs/scanf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+  int num; 
+  float price; 
+
+  //Input and Output
+  printf("Enter a Number: ");
+  scanf("%d", &num);
+  printf("num = %d", num);
+
+  printf("\nEnter a Number: ");
+  scanf("%f", &price);
+  printf("num = %f", price);
+
+  return 0;
+}
